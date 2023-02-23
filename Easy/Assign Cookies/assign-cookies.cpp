7be39d2e95116ff62732ab695d2b7a9be3152b56
srcode03 <99495910+srcode03@users.noreply.github.com>
2023-02-23 21:10:53 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Assign Cookies/assign-cookies.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxChildren(int N, int M, vector<int> &greed, vector<int> &sz) {
+        // code here
+        sort(greed.begin(),greed.end());
+        sort(sz.begin(),sz.end());
+        int ct=0;
+        int i=greed.size()-1;
+        int j=sz.size()-1;
+        while(i>=0 && j>=0)
+        {
+            if(greed[i]<=sz[j])
+            {
+                ct++;
+                i--;
+                j--;
+            }
+            else{
+              i--;
+            }
+        }
+        return ct;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N, M;
+        cin >> N >> M;
+
+        vector<int> greed(N), sz(M);
+        for (int i = 0; i < N; i++) {
+            cin >> greed[i];
+        }
+        for (int i = 0; i < M; i++) {
+            cin >> sz[i];
+        }
+        Solution obj;
+        cout << obj.maxChildren(N, M, greed, sz) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
