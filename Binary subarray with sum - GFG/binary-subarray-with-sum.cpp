dc94de160dd53322f66a52af6a30ca5acda5adb6
srcode03 <99495910+srcode03@users.noreply.github.com>
2023-06-12 19:47:48 +0530

    Added solution - LeetHub

---
+++ Binary subarray with sum - GFG/binary-subarray-with-sum.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int numberOfSubarrays(vector<int>& arr, int N, int target){
+        // code here
+        map<int,int>mp;
+        mp[0]++;
+        int x=0;
+        int sum=0;
+        for(auto it:arr)
+        {
+            sum=sum+it;
+            x=x+mp[sum-target];
+            mp[sum]++;
+        }
+        return x;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int target; 
+        cin >> target;
+        vector<int> arr(N);
+        for(int i=0; i<N; i++)
+            cin >> arr[i];
+        Solution obj;
+        cout<<obj.numberOfSubarrays(arr, N, target)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
