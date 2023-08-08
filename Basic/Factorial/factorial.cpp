18a53d74f885e0ce3f4b7939f09717a2428e4d48
srcode03 <99495910+srcode03@users.noreply.github.com>
2023-08-08 10:28:10 +0530

    Added Solution - GfG to GitHub

---
+++ Basic/Factorial/factorial.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+public:
+    long long int factorial(int N){
+        //code here
+        if(N==1 || N==0)
+        {
+            return 1;
+        }
+        return factorial(N-1)*N;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.factorial(N) << endl;
+    }
+    return 0; 
+}
+// } Driver Code Ends
