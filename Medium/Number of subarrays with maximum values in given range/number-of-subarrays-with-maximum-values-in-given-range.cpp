85de2d0c3e4d6d4a816b5e4ae947853fe1366836
srcode03 <99495910+srcode03@users.noreply.github.com>
2023-12-07 08:51:27 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of subarrays with maximum values in given range/number-of-subarrays-with-maximum-values-in-given-range.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution{
+    public:
+    long countSubarrays(int a[], int n, int L, int R)
+    {
+        // Complete the function
+        long cnt = 0;
+        long size = 0;
+        
+        // counting the cases where the maximum elem is smaller than or equal to R
+        for(int i=0; i<n; i++) {
+            if(a[i] <= R) {
+                while(a[i] <= R and i < n) {
+                    size++;
+                    i++;
+                }
+            }
+            
+            cnt += (size * (size + 1))/2;
+            if(a[i] > R)
+                size = 0;
+        }
+        
+        // removing the cases where the maximum elem is less than L
+        size = 0;
+        for(int i=0; i<n; i++) {
+            if(a[i] < L) {
+                while(a[i] < L and i < n) {
+                    size++;
+                    i++;
+                }
+            }
+            
+            cnt -= (size * (size + 1))/2;
+            if(a[i] >= L)
+                size = 0;
+        }
+        
+        return cnt;
+    }
+};
+
+//{ Driver Code Starts.
+// driver program
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        
+        int n ,l,r;
+        cin >> n>>l>>r;
+        int a[n];
+        for(int i=0;i<n;i++)
+        cin >> a[i];
+        Solution ob;
+    	cout << ob.countSubarrays(a, n, l, r)<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
