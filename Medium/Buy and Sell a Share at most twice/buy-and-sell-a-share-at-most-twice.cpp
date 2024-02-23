265f7a3b98a93f5d0d761c99c8709fc5ce33834c
srcode03 <99495910+srcode03@users.noreply.github.com>
2024-02-23 10:30:50 +0530

    Updated Solution - GfG to GitHub

--- Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
+++ Medium/Buy and Sell a Share at most twice/buy-and-sell-a-share-at-most-twice.cpp
@@ -5,30 +5,31 @@ using namespace std;
 
 
 // } Driver Code Ends
-//User function Template for C++
 
-int maxProfit(vector<int>&price){
-    //Write your code here..
-    int n=price.size();
-    vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-    for(int i=n-1;i>=0;i--)
-    {
-        for(int j=0;j<=1;j++)
-        {
-            for(int k=1;k<=2;k++)
-            {
-                if(j==1)
-                {
-                    dp[i][j][k]=max(-price[i]+dp[i+1][0][k],dp[i+1][1][k]);
-                }
-                else{
-                    dp[i][j][k]=max(price[i]+dp[i+1][1][k-1],dp[i+1][0][k]);
+class Solution
+{
+    public:
+        //User function Template for C++
+    int maxProfit(vector<int>&price){
+            //Write your code here..
+            long long n=price.size();
+            vector<vector<vector<long long>>>dp(n+1,vector<vector<long long>>(2,vector<long long>(3,0)));
+            for(long long ind=n-1;ind>=0;ind--){
+                for(int buy=0;buy<=1;buy++){
+                    for(int cap=1;cap<=2;cap++){
+                        if(buy==1){
+                              dp[ind][buy][cap]=max(-price[ind]+dp[ind+1][0][cap],0+dp[ind+1][1][cap]);
+                        }
+                        else{
+                              dp[ind][buy][cap]=max(price[ind]+dp[ind+1][1][cap-1],0+dp[ind+1][0][cap]);
+                        }
+                    }
                 }
             }
+            return dp[0][1][2];
+                
         }
-    }
-    return dp[0][1][2];
-}
+};
 
 //{ Driver Code Starts.
 
@@ -41,7 +42,8 @@ int main(){
         cin>>n;
         vector<int> price(n);
         for(int i=0;i<n;i++) cin>>price[i];
-        cout<<maxProfit(price)<<endl;
+        Solution obj;
+        cout<<obj.maxProfit(price)<<endl;
     }
 
 }
