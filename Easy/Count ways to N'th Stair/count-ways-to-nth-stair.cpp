bfbe0ae6f559503ae29878b2fc432ad8406e3024
srcode03 <99495910+srcode03@users.noreply.github.com>
2024-04-06 09:23:36 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Count ways to N'th Stair/count-ways-to-nth-stair.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+  public:
+    long long countWays(int n)
+    {
+        // your code here
+        return n/2+1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    // taking count of testcases
+    int t;
+    cin >> t;
+
+    while (t--) {
+        // taking stair count
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.countWays(n) << endl; // Print the output from our pre computed array
+    }
+    return 0;
+}
+// } Driver Code Ends
