dbcc384d3c5d5425b5866d62209086edc9076205
srcode03 <99495910+srcode03@users.noreply.github.com>
2024-08-24 23:21:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Minimum difference pair/minimum-difference-pair.cpp
@@ -0,0 +1,35 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+   	int  minimum_difference(vector<int>nums){
+   	    // Code here
+   	    sort(nums.begin(),nums.end());
+   	    int mini=INT_MAX;
+   	    for(int i=1;i<nums.size();i++)
+   	    {
+   	        mini=min(mini,abs(nums[i]-nums[i-1]));
+   	    }
+   	    return mini;
+   	}    
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		vector<int>nums(n);
+		for(int i = 0; i < nums.size(); i++)cin >> nums[i];
+		Solution ob;
+		int ans = ob.minimum_difference(nums);
+		cout << ans <<"\n";
+	}  
+	return 0;
+}
+// } Driver Code Ends
